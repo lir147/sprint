5de0776ad4da8b0f6979bb60bd2205b2e452c431
lir147 <120119304+lir147@users.noreply.github.com>
2025-09-18 19:03:25 +0500

    Все зависимости

--- fstr_pereval_project/app.py
+++ fstr_pereval_project/app.py
@@ -296,4 +296,5 @@ def get_image(image_id):
 
 # ----------------- Запуск приложения -----------------
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=int(os.getenv("PORT", 8080)), debug=True)
+
